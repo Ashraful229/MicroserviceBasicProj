ee0ec26a7e81f4c671db57c32e281b668bf102b6
Ashraful Alam <emonak52@gmail.com>
2022-01-27 13:51:32 +0600

    api gateway and hystrix CircuitBreaker done

---
+++ cloud-gateway/src/main/java/com/javatechie/cloud/gateway/FallbackController.java
@@ -0,0 +1,22 @@
+package com.javatechie.cloud.gateway;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FallbackController {
+
+    @RequestMapping("/orderFallBack")
+    public Mono<String> orderServiceFallBack()
+    {
+        System.out.println("okokokok");
+        return Mono.just("Oder Service May Be Down");
+    }
+
+    @RequestMapping("/paymentFallBack")
+    public Mono<String> paymentServiceFallBack()
+    {  System.out.println("okokokok2");
+        return Mono.just("Payment Service May Be Down");
+    }
+}
